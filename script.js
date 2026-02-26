0882467ef68c7f258bf6b6e7bbcbecc1b6377606
Gustavo Avelino Sanchez <160865518+gusanchez1@users.noreply.github.com>
2026-02-26 14:18:43 -0300

    att site

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+const link = document.querySelector(".links");
+
 hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+link.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
