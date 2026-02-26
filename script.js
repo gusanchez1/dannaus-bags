27a0bbf590d2d88e6329136a897750e490b9727c
Gustavo Avelino Sanchez <160865518+gusanchez1@users.noreply.github.com>
2026-02-26 15:04:06 -0300

    JS

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 const links = document.querySelectorAll(".links");
 
+function closeMenu() {
+  hamMenu.classList.remove("active");
+  offScreenMenu.classList.remove("active");
+  document.body.classList.remove("no-scroll");
+}
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-  document.body.classList.toggle("no-scroll")
+  document.body.classList.toggle("no-scroll");
 });
 
 links.forEach(link => {
-  link.addEventListener("click", () => {
-    hamMenu.classList.remove("active");
-    offScreenMenu.classList.remove("active");
-    document.body.classList.remove("no-scroll");
-  });
+  link.addEventListener("click", closeMenu);
 });
